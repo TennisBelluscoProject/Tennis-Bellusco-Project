289a8bbb0834902546c116f9e4f59bbb78b43150
SimonePreda <app.tennisbellusco@gmail.com>
2026-06-11 17:10:53 +0200

    fix scrolla bar catalogo mobile

--- app/coach/tabs/CatalogoTab.tsx
+++ app/coach/tabs/CatalogoTab.tsx
@@ -28,7 +28,7 @@ export function CatalogoTab({ coachId }: Props) {
           </button>
         ))}
       </div>
-      <div className="flex-1 min-h-0">
+      <div className="flex-1 min-h-0 flex flex-col">
         {view === 'obiettivi' ? (
           <GoalTemplateManager coachId={coachId} />
         ) : (
