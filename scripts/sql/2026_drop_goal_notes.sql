59d2e61f9e17d75ce7f3435482bf56de0bcec315
TennisBelluscoProject <app.tennisbellusco@gmail.com>
2026-06-26 00:46:21 +0200

    Delete scripts directory

--- scripts/sql/2026_drop_goal_notes.sql
+++
@@ -1,4 +0,0 @@
--- Rimozione tabella goal_notes (mai utilizzata: 0 righe in produzione,
--- nessuna query nel codice, solo dichiarata nei tipi TS).
-
-drop table if exists public.goal_notes cascade;
